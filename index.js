8a6980041c257d87002b6aac324ff2e61165e7f0
Phillip <117806991+tddan6@users.noreply.github.com>
2023-05-10 23:26:52 +1000

    Update index.js

--- index.js
+++ index.js
@@ -27,16 +27,26 @@ document.querySelector("#hacker-effect").onmouseover = event => {
 }
 
 
-// const sr = ScrollReveal ({
-//     distance: '65px',
-//     duration: 2600,
-//     delay: 450,
-//     reset: true
-// });
+const sr = ScrollReveal ({
+    distance: '65px',
+    duration: 2600,
+    delay: 450,
+    reset: false
+});
 
-// sr.reveal('.landing-heading',{delay:200, origin:'top'});
-// sr.reveal('.left-container p',{delay:450, origin:'top'});
-// sr.reveal('.right-container',{delay:600, origin:'right'});
-// sr.reveal('.secondary-content',{delay:800, origin:'left'});
+sr.reveal('.landing-heading',{delay:100, origin:'top'});
+sr.reveal('.left-container p',{delay:350, origin:'top'});
+sr.reveal('.coming-soon',{delay:400, origin:'top'});
+sr.reveal('.right-container',{delay:500, origin:'right'});
+sr.reveal('.secondary-content',{delay:700, origin:'left'});
+sr.reveal('.fancy-text',{delay:600, origin:'left'});
+sr.reveal('.polaroid-2',{delay:400, origin:'right'});
+sr.reveal('.polaroid-1',{delay:500, origin:'left'});
+sr.reveal('#note-1',{delay:500, origin:'top'});
+sr.reveal('#note-2',{delay:550, origin:'top'});
+sr.reveal('#note-3',{delay:600, origin:'top'});
+sr.reveal('#note-4',{delay:650, origin:'top'});
+sr.reveal('#note-5',{delay:700, origin:'top'});
+sr.reveal('.bingsoo-sticker-1',{delay:800, origin:'right'});
 
 
